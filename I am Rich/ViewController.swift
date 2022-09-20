4bf02094aba67f6d4e786fa3bfa689d2e8a424b2
Deniz Ata ES <denizataes@hotmail.com>
2022-09-20 23:33:45 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Deniz Ata Eş on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am RichUITests/I_am_RichUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_am_RichUITestsLaunchTests.swift
+//  I am RichUITests
+//
+//  Created by Deniz Ata Eş on 20.09.2022.
+//
+
+import XCTest
+
+final class I_am_RichUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
